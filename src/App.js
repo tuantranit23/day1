8b1df9fb8f9d754915a007b2f9077457968a2cad
Tuan <tuantranit23@gmail.com>
2021-06-01 20:53:03 +0700

    update 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello word wtf hahahas 1s 123
         </a>
       </header>
     </div>
